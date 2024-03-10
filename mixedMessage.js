c2d751c9a0b3741235d9079b56773120c023751b
ConnorRyall <conryall@gmail.com>
2024-03-10 18:26:52 +0100

    added trip details variables

--- mixedMessage.js
+++ mixedMessage.js
@@ -2,6 +2,11 @@
 const trip = {
 tripLocation: ['Austrailia', 'Bali', 'Spain', 'Vietnam', 'Iceland', 'Ireland', 'Zimbabwe', 'Norway', 'South Korea', 'Brazil', 'Canada', 'Antarctica'],
 tripSeason: ['summer', 'fall', 'winter', 'spring'],
-tripType = ['solo', 'with friends', 'with family']
+tripType: ['solo', 'with friends', 'with family']
 };
-//
+
+//Create function to choose random element from arrays
+const tripLocation = trip.tripLocation[Math.floor(Math.random() * trip.tripLocation.length)];
+const tripSeason = trip.tripSeason[Math.floor(Math.random() * trip.tripSeason.length)];
+const tripType = trip.tripType[Math.floor(Math.random() * trip.tripType.length)];
+
