beb8525a5a9a6992771974638b8d5b84dcaf9bd8
ConnorRyall <conryall@gmail.com>
2024-03-10 18:34:40 +0100

    Basic version finished

--- mixedMessage.js
+++ mixedMessage.js
@@ -5,8 +5,17 @@ tripSeason: ['summer', 'fall', 'winter', 'spring'],
 tripType: ['solo', 'with friends', 'with family']
 };
 
-//Create function to choose random element from arrays
+//Create trip variables to hold random elements from arrays
 const tripLocation = trip.tripLocation[Math.floor(Math.random() * trip.tripLocation.length)];
 const tripSeason = trip.tripSeason[Math.floor(Math.random() * trip.tripSeason.length)];
 const tripType = trip.tripType[Math.floor(Math.random() * trip.tripType.length)];
 
+//Create variable to hold trip variables
+const tripData = [tripLocation, tripSeason, tripType];
+
+//Create formatted output message 
+const message = name => {
+    return `Hello ${name}! You should go to ${tripLocation} in ${tripSeason} ${tripType}.`;
+}
+//Execute message
+console.log(message('Connor'));
