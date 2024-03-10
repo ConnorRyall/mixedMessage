11a3ec3baead419dca4c8350cc8c8674a7b7ab35
ConnorRyall <conryall@gmail.com>
2024-03-10 18:17:40 +0100

    changed arrays to object

--- mixedMessage.js
+++ mixedMessage.js
@@ -1,4 +1,7 @@
 //Create arrays for trip location, season and type
-const tripLocation = ['Austrailia', 'Bali', 'Spain', 'Vietnam', 'Iceland', 'Ireland', 'Zimbabwe', 'Norway', 'South Korea', 'Brazil', 'Canada', 'Antarctica'];
-const tripSeason = ['summer', 'fall', 'winter', 'spring'];
-const tripType = ['solo', 'with friends', 'with family'];
+const trip = {
+tripLocation: ['Austrailia', 'Bali', 'Spain', 'Vietnam', 'Iceland', 'Ireland', 'Zimbabwe', 'Norway', 'South Korea', 'Brazil', 'Canada', 'Antarctica'],
+tripSeason: ['summer', 'fall', 'winter', 'spring'],
+tripType = ['solo', 'with friends', 'with family']
+};
+//
