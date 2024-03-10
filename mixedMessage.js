904f8e97cb320c8bfd0fbc847d75ec380018c386
ConnorRyall <conryall@gmail.com>
2024-03-10 18:09:51 +0100

    First commit

---
+++ mixedMessage.js
@@ -0,0 +1,4 @@
+//Create arrays for trip location, season and type
+const tripLocation = ['Austrailia', 'Bali', 'Spain', 'Vietnam', 'Iceland', 'Ireland', 'Zimbabwe', 'Norway', 'South Korea', 'Brazil', 'Canada', 'Antarctica'];
+const tripSeason = ['summer', 'fall', 'winter', 'spring'];
+const tripType = ['solo', 'with friends', 'with family'];
